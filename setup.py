4d03643c71deeefad4cd062dc0f1fffc7bc5bd0b
Maurits van Rees <maurits@vanrees.org>
2023-03-14 16:01:54 +0100

    Drop support for Python 2.

--- setup.py
+++ setup.py
@@ -32,7 +32,6 @@ setup(name='collective.cookiecuttr',
         "Framework :: Plone :: 5.2",
         "Framework :: Plone :: 6.0",
         "Programming Language :: Python",
-        "Programming Language :: Python :: 2.7",
         "Programming Language :: Python :: 3.7",
         "Programming Language :: Python :: 3.8",
         "Programming Language :: Python :: 3.9",
@@ -51,7 +50,9 @@ setup(name='collective.cookiecuttr',
       namespace_packages=['collective', ],
       include_package_data=True,
       zip_safe=False,
-      python_requires=">=2.7,!=3.0.*,!=3.1.*,!=3.2.*,!=3.3.*,!=3.4.*,!=3.5.*",
+      # Py 3.6 is end of life, but I don't want to explicitly break it
+      # by disallowing it here.
+      python_requires=">=3.6",
       install_requires=[
           'setuptools',
           'plone.app.registry',
